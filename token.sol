9e51f522deda678bb6390b97be9eabbe68096093
Nivedita Srivastava <112811084+Niveditasri@users.noreply.github.com>
2024-06-07 16:16:04 +0530

    Update token.sol

--- token.sol
+++ token.sol
@@ -11,40 +11,29 @@ pragma solidity 0.8.25;
    5. Lastly, your burn function should have conditionals to make sure the balance of account is greater than or equal 
    to the amount that is supposed to be burned.
 */
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
 contract MyToken {
 
-    // Public variables to store token details
-    string public name;
-    string public symbol;
-    uint256 public totalSupply;
+    // Public variables to store the details about the coin
+    string public name = "MyToken";
+    string public symbol = "MTK";
+    uint256 public totalSupply = 0;
 
-    // Mapping from addresses to their balances
+    // Mapping of addresses to balances
     mapping(address => uint256) public balances;
 
-    // Constructor to initialize the token details
-    constructor(string memory _name, string memory _symbol, uint256 _initialSupply) {
-        name = _name;
-        symbol = _symbol;
-        totalSupply = _initialSupply;
-        balances[msg.sender] = _initialSupply; // Assign the entire initial supply to the contract deployer
+    // Mint function to increase the total supply and balance of the sender
+    function mint(address _to, uint256 _value) public {
+        totalSupply += _value;
+        balances[_to] += _value;
     }
 
-    // Mint function to create new tokens
-    function mint(address _to, uint256 _amount) public {
-        require(_to != address(0), "Cannot mint to the zero address");
-        totalSupply += _amount;
-        balances[_to] += _amount;
-        emit Transfer(address(0), _to, _amount);
+    // Burn function to decrease the total supply and balance of the sender
+    function burn(address _from, uint256 _value) public {
+        require(balances[_from] >= _value, "Insufficient balance to burn");
+        totalSupply -= _value;
+        balances[_from] -= _value;
     }
-
-    // Burn function to destroy tokens
-    function burn(address _from, uint256 _amount) public {
-        require(balances[_from] >= _amount, "Burn amount exceeds balance");
-        totalSupply -= _amount;
-        balances[_from] -= _amount;
-        emit Transfer(_from, address(0), _amount);
-    }
-
-    // Event to log token transfers
-    event Transfer(address indexed from, address indexed to, uint256 value);
 }
