4747c920c37015d3b12790870a8b3a9c32b271bc
Nivedita Srivastava <112811084+Niveditasri@users.noreply.github.com>
2024-06-10 11:49:26 +0530

    Update token.sol

--- token.sol
+++ token.sol
@@ -1,36 +1,53 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.25;
+pragma solidity 0.8.18;
 
-/* 1. Your contract will have public variables that store the details about your coin (Token Name, Token Abbrv., Total Supply)
-   2. Your contract will have a mapping of addresses to balances (address => uint)
-   3. You will have a mint function that takes two parameters: an address and a value.
-   The function then increases the total supply by that number and increases the balance of the address by that amount.
-   4. Your contract will have a burn function, which works the opposite of the mint function, as it will destroy tokens. 
-   It will take an address and value just like the mint functions. 
-   It will then deduct the value from the total supply and from the balance of the address.
-   5. Lastly, your burn function should have conditionals to make sure the balance of account is greater than or equal 
-   to the amount that is supposed to be burned.
+/*
+       REQUIREMENTS
+    1. Your contract will have public variables that store the details about your coin (Token Name, Token Abbrv., Total Supply)
+    2. Your contract will have a mapping of addresses to balances (address => uint)
+    3. You will have a mint function that takes two parameters: an address and a value. 
+       The function then increases the total supply by that number and increases the balance 
+       of the “sender” address by that amount
+    4. Your contract will have a burn function, which works the opposite of the mint function, as it will destroy tokens. 
+       It will take an address and value just like the mint functions. It will then deduct the value from the total supply 
+       and from the balance of the “sender”.
+    5. Lastly, your burn function should have conditionals to make sure the balance of "sender" is greater than or equal 
+       to the amount that is supposed to be burned.
 */
+
 contract MyToken {
 
-    // Public variables to store the details about the coin
-    string public name = "MyToken";
-    string public symbol = "MTK";
-    uint256 public totalSupply = 0;
+    // public variables here
+    string public name;
+    string public symbol;
+    uint256 public totalSupply;
 
-    // Mapping of addresses to balances
+    // mapping variable here
     mapping(address => uint256) public balances;
 
-    // Mint function to increase the total supply and balance of the sender
+    // constructor to initialize the token details
+    constructor(string memory _name, string memory _symbol, uint256 _initialSupply) {
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _initialSupply;
+        balances[msg.sender] = _initialSupply;
+    }
+
+    // mint function
     function mint(address _to, uint256 _value) public {
         totalSupply += _value;
         balances[_to] += _value;
     }
 
-    // Burn function to decrease the total supply and balance of the sender
+    // burn function
     function burn(address _from, uint256 _value) public {
         require(balances[_from] >= _value, "Insufficient balance to burn");
         totalSupply -= _value;
         balances[_from] -= _value;
+    }
+
+    // function to get the balance of a specific address
+    function balanceOf(address _account) public view returns (uint256) {
+        return balances[_account];
     }
 }
